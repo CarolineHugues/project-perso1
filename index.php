98a07e8161bc4a2dd2406f3de51c42178ea3d86c
Caroline Hugues <hugues.caroline9@gmail.com>
2019-03-02 11:29:58 +0100

    first version added

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Musique & voyages</title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="stylesheets/main.css">
+	<link href="https://fonts.googleapis.com/css?family=Srisakdi" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet"> 
+</head>
+<body>
+	<header>
+		<h1>Musique & voyages</h1>
+	</header>
+	<section>
+		<?php include 'edinburgh-article.php'; ?>
+		<?php include 'dublin-article.php'; ?>
+		<?php include 'praha-article.php'; ?>
+	</section>
+	<footer>
+		<p>Projet personnel en SASS - Caroline Hugues</p>
+	</footer>
+</body>
+</html>
