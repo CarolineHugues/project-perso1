bcb3f5c07e68c7fcf41512c069b2ee1309f2872a
Caroline Hugues <hugues.caroline9@gmail.com>
2019-03-13 23:22:43 +0100

    Script and stylesheets for openstreetmap declared

---
+++ footer.php
@@ -0,0 +1,11 @@
+	<footer>
+		<p id="text-footer">Projet personnel en SASS - Caroline Hugues</p>
+		<script src="js/ajax.js"></script>
+		<script src="js/slideshow.js"></script>
+		<script src="js/playslideshow.js"></script>
+		<script src="js/map.js"></script>
+		<script type='text/javascript' src='https://unpkg.com/leaflet@1.3.1/dist/leaflet.js' integrity="sha512-/Nsx9X4HebavoBvEBuyp3I7od5tA0UzAxs+j83KgC8PU0kgB4XiK4Lfe4y4cgBtaRJQEIFCW+oC506aPT2L1zw==" crossorigin=""></script>
+    	<script type='text/javascript' src='https://unpkg.com/leaflet.markercluster@1.3.0/dist/leaflet.markercluster.js'></script>
+	</footer>
+</body>
+</html>
