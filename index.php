52a2c73a2b516fd6d9fa717b0c5298b234a628ba
Caroline Hugues <hugues.caroline9@gmail.com>
2019-03-13 23:21:08 +0100

    Files added

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php include 'header.php'; ?>
+<section>
+	<?php include 'carousel.php'; ?>
+</section>
+<section>
+	<div id="map"></div>
+</section>
+<?php include 'footer.php'; ?>
---
+++ javascript/map.js
@@ -0,0 +1,29 @@
+$( function() { 
+  
+  function initMap() {
+    $.getJSON('http://localhost/project-sass-music-travel/api/articles-data.php', function( data ) {
+      console.log(data);
+      var myMap = null;
+      var markerClusters;
+      var markers = []; 
+      myMap = L.map('map').setView([48.852969, 2.349903], 11);
+      markerClusters = L.markerClusterGroup(); 
+      L.tileLayer('https://{s}.tile.openstreetmap.fr/osmfr/{z}/{x}/{y}.png', {
+        attribution: 'données © <a href="//osm.org/copyright">OpenStreetMap</a>/ODbL - rendu <a href="//openstreetmap.fr">OSM France</a>',
+        minZoom: 1,
+        maxZoom: 20
+      }).addTo(myMap);
+      for (var i = 0; i < data.result.articles.length; i++) {
+        var marker = L.marker([data.result.articles[i].lat, data.result.articles[i].lon]);
+        marker.bindPopup('<a href="http://localhost/project-sass-music-travel/article.php?id=/' + data.result.articles[i].id + '">' + data.result.articles[i].title + '</a>' + '<br/> ' + data.result.articles[i].description + '<br/> ' + '<img src="' + data.result.articles[i].image + '">');
+        markerClusters.addLayer(marker); 
+        markers.push(marker);
+      } 
+      var group = new L.featureGroup(markers); 
+      myMap.fitBounds(group.getBounds().pad(0.5)); 
+      myMap.addLayer(markerClusters); 
+    });
+  }
+
+  initMap();   
+});
