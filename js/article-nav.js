ebf88ec233112eda4833cd3b1e80077ffc995a4f
Caroline Hugues <hugues.caroline9@gmail.com>
2020-04-13 13:15:32 +0200

    feat(article nav) : navigation closed at scroll

--- js/article-nav.js
+++ js/article-nav.js
@@ -2,3 +2,8 @@ document.getElementById('arrow-nav').addEventListener('click', function() {
     document.querySelector('#nav-links').classList.toggle('hide');
     this.classList.toggle('close');
 })
+
+document.addEventListener('scroll', function() {
+    document.querySelector('#nav-links').classList.add('hide');
+    document.getElementById('arrow-nav').classList.remove('close');
+})
